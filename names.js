3b1b18564f1984111ba77a811702f60fe480b899
NavodContrath <navod.contrath@gmail.com>
2025-03-10 15:44:18 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,9 @@
+/* 
+1.names.js
+Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+Esporta la funzione dal file.
+*/
+function nameAdder(name, surname) {
+    let fullName = { firstName: name, lastName: surname }
+    return fullName
+}
