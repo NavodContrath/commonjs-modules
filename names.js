25d2c05746fe5ff467b907d03e6b1b27bfc09413
NavodContrath <navod.contrath@gmail.com>
2025-03-10 15:46:19 +0100

    add function's description

--- names.js
+++ names.js
@@ -3,6 +3,12 @@
 Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 Esporta la funzione dal file.
 */
+/**
+ * 
+ * @param {*} name 
+ * @param {*} surname 
+ * @returns fullName
+ */
 function nameAdder(name, surname) {
     let fullName = { firstName: name, lastName: surname }
     return fullName
