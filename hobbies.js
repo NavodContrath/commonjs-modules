b2149116070e8adb6a126916e14eee8832c4578e
NavodContrath <navod.contrath@gmail.com>
2025-03-10 15:50:56 +0100

    add function hobbiesAdder

--- hobbies.js
+++ hobbies.js
@@ -3,3 +3,14 @@
 Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un array di stringhe contenenti gli hobby.
 Esporta la funzione dal file.
  */
+/**
+ * 
+ * @param {*} hobbyOne 
+ * @param {*} hobbyTwo 
+ * @param {*} hobbyThree 
+ * @returns array of hobbies
+ */
+function hobbiesAdder(hobbyOne, hobbyTwo, hobbyThree) {
+    let hobbiesArr = [hobbyOne, hobbyTwo, hobbyThree]
+    return hobbiesAdder
+}
